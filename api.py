042080e5adb6cbd942fb5275f9ca7712620825c2
lauralcodes <charish.weerasuriya@gmail.com>
2026-09-26 00:49:47 +0200

    improve event deletion function with validation.

--- api.py
+++ api.py
@@ -32,11 +32,6 @@ def create_event():
             "error": "Request body must contain JSON"
         }), 400
 
-    # To check whether the event ID already exists
-    if event_exists(event["event_id"]):
-        return jsonify({
-             "error": "Event ID already exists"
-         }), 400
 
     # add validation to make sure all the required fields are sent, and validate the price, date and time
     error = validate_event(event)
@@ -45,7 +40,11 @@ def create_event():
             "error": error
         }), 400
 
-
+    # To check whether the event ID already exists
+    if event_exists(event["event_id"]):
+        return jsonify({
+            "error": "Event ID already exists"
+        }), 400
 
     add_event(event)
 
